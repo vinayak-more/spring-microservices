cab8788159b3c8936c8dd0e4497b31901d4d11ed
Vinayak More <vinayak.more@infibeam.net>
2018-06-06 10:54:29 +0530

    hello world controller added

---
+++ restful-service/src/main/java/com/vinayak/restfulservice/helloworld/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.vinayak.restfulservice.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping("/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	@GetMapping("/hello-world/{pathVariable}")
+	public String helloWorldPathVariable(@PathVariable String pathVariable) {
+		return String.format("Hello World %s", pathVariable);
+	}
+}
